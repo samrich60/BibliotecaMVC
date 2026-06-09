df96fe445e055e80432d4f3b27ca442e11fcffcc
Sammy_Richard <simonesammy9@gmail.com>
2026-06-09 19:40:44 -0300

    criando o layout base, navbar, footer, home e sobre

--- BibliotecaMVC/Controllers/HomeController.cs
+++ BibliotecaMVC/Controllers/HomeController.cs
@@ -1,32 +1,27 @@
-using BibliotecaMVC.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace BibliotecaMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Sobre()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Acervo()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
+        }
+
+        public IActionResult Contato()
+        {
+            return View();
         }
     }
 }
