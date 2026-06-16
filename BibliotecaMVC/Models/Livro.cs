412bee9c908146d5c739975dcd2498cde80cacab
Sammy_Richard <simonesammy9@gmail.com>
2026-06-16 16:44:01 -0300

    fazendo páginas acervo e contato e adicionando o model livro

---
+++ BibliotecaMVC/Models/Livro.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibliotecaMVC.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O título é obrigatório")]
+        [Display(Name = "Título")]
+        public string Titulo { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O autor é obrigatório")]
+        [Display(Name = "Autor")]
+        public string Autor { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "A categoria é obrigatória")]
+        [Display(Name = "Categoria")]
+        public string Categoria { get; set; } = string.Empty;
+
+        [Display(Name = "Ano de Publicação")]
+        public int AnoPublicacao { get; set; }
+
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; } = string.Empty;
+
+        [Display(Name = "Disponível")]
+        public bool Disponivel { get; set; } = true;
+
+        [Display(Name = "ISBN")]
+        public string ISBN { get; set; } = string.Empty;
+    }
+}
---
+++ BibliotecaMVC/Views/Home/Acervo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BibliotecaMVC.Views.Home
+{
+    public class AcervoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
